f2863d578a6a41a72aa088061577c86c5674b965
morisoto <k.morisoto@quo-digital.jp>
2020-11-19 16:37:45 +0900

    try flyway

---
+++ utilities/src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE customer
+(
+    customer_id integer               NOT NULL,
+    store_id    smallint              NOT NULL,
+    first_name  character varying(45) NOT NULL,
+    last_name   character varying(45) NOT NULL
+);
